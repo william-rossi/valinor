61882390154f9a8b0b17fbf2567248a38ffdda90
William Ruiz <71782127+william-rossi@users.noreply.github.com>
2023-06-30 10:19:57 -0300

    Estilo para página selecionada

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -7,11 +7,13 @@ interface IPagination {
 }
 
 const Pagination = (props: IPagination) => {
-
     return (
         <div className={styles.content}>
             {props.pageNumbers.map((index) => (
                 <button
+                    style={index === props.pageNumber 
+                    ? { backgroundColor: "#9DB2BF", color: "#27374D", border: "2px solid #27374D", fontWeight: 'bold', cursor: 'not-allowed' } 
+                    : {}}
                     key={index}
                     onClick={() => props.clickOnSelectedPage(index)}
                     disabled={index === props.pageNumber}
