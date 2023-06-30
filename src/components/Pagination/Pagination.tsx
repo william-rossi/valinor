f0378f681abb0f7b5ce8473c03991d135cd0b941
William Ruiz <71782127+william-rossi@users.noreply.github.com>
2023-06-30 10:36:26 -0300

    Adiciona classe para página atual selecionada

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -11,9 +11,7 @@ const Pagination = (props: IPagination) => {
         <div className={styles.content}>
             {props.pageNumbers.map((index) => (
                 <button
-                    style={index === props.pageNumber 
-                    ? { backgroundColor: "#9DB2BF", color: "#27374D", border: "2px solid #27374D", fontWeight: 'bold', cursor: 'not-allowed' } 
-                    : {}}
+                    className={index === props.pageNumber ? styles.currentPage : ''}
                     key={index}
                     onClick={() => props.clickOnSelectedPage(index)}
                     disabled={index === props.pageNumber}
